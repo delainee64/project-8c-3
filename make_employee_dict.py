62e34775e1b1a84229fd24f92604e0fff2547876
Delainee Lenss <lenssd@oregonstate.edu>
2022-11-12 20:31:33 -0600

    Commit Message

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -9,26 +9,26 @@ class Employee:
 
     def __init__(self, name, id_number, salary, email):
         """Defines employee information"""
-        self.__name = name
-        self.__ID_number = id_number
-        self.__salary = salary
-        self.__email_address = email
+        self._name = name
+        self._ID_number = id_number
+        self._salary = salary
+        self._email_address = email
 
     def get_name(self):
         """Returns the name of the employee"""
-        return self.__name
+        return self._name
 
     def get_ID_number(self):
         """Returns the ID number of the employee"""
-        return self.__ID_number
+        return self._ID_number
 
     def get_salary(self):
         """Returns the salary of the employee"""
-        return self.__salary
+        return self._salary
 
     def get_email_address(self):
         """Returns the email of the employee"""
-        return self.__email_address
+        return self._email_address
 
 
 def make_employee_dict(emp_names, emp_ids, emp_sals, emp_emails):
@@ -42,11 +42,11 @@ def make_employee_dict(emp_names, emp_ids, emp_sals, emp_emails):
     return employee_dict
 
 
-# emp_names = ["Jean", "Kat", "Pomona"]
-# emp_ids = ["100", "101", "102"]
-# emp_sals = [30, 35, 28]
-# emp_emails = ["Jean@aol.com", "Kat@aol.com", "Pomona@aol.com"]
-# result_dictionary = make_employee_dict(emp_names, emp_ids, emp_sals, emp_emails)
+emp_names = ["Jean", "Kat", "Pomona"]
+emp_ids = ["100", "101", "102"]
+emp_sals = [30, 35, 28]
+emp_emails = ["Jean@aol.com", "Kat@aol.com", "Pomona@aol.com"]
+result_dictionary = make_employee_dict(emp_names, emp_ids, emp_sals, emp_emails)
 
 # for key, value in result_dictionary.items():
 # print(key, value.get_name(), value.get_salary(), value.get_email_address())
